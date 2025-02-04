5c82fa481fc13c815eedd5d4c6593987f9d104ce
iamrazzakk <rakibt23p@gmail.com>
2025-02-04 17:25:22 +0600

    create service and controller for event

--- src/app/modules/event/event.controller.ts
+++ src/app/modules/event/event.controller.ts
@@ -1,4 +1,25 @@
-import { Request, Response, NextFunction } from 'express';
-import { EventServices } from './event.service';
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import { Event } from "./event.model";
+import { StatusCodes } from "http-status-codes";
+import sendResponse from "../../../shared/sendResponse";
+import { EventServices } from "./event.service";
 
-export const EventController = { };
+
+const createEvent = catchAsync(async (req: Request, res: Response) => {
+    const eventData = req.body;
+    const result = await EventServices.createEventIntoDB(eventData);
+    sendResponse(res, {
+        success: true,
+        statusCode: StatusCodes.OK,
+        message: 'Event created successfully',
+        data: result,
+    });
+});
+
+
+
+export const EventController = {
+    createEvent,
+
+};
--- src/app/modules/event/event.service.ts
+++ src/app/modules/event/event.service.ts
@@ -1,3 +1,22 @@
+import { StatusCodes } from 'http-status-codes';
+import ApiError from '../../../errors/ApiError';
 import { EventModel } from './event.interface';
+import { Event } from './event.model';
 
-export const EventServices = { };
+const createEventIntoDB = async (eventData: EventModel) => {
+    try {
+        const event = (await Event.create(eventData));
+        if (!event) {
+            throw new ApiError(StatusCodes.BAD_REQUEST, "Failed to create the event");
+        }
+
+        return event
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+export const EventServices = {
+    createEventIntoDB
+};
