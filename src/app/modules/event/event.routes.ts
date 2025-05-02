9cf59964fa854e2554e1c21900c031892d73c198
iamrazzakk <rakibt23p@gmail.com>
2025-05-02 17:47:15 +0600

    feat(event): add ADMIN role to event route permissions
    Extend route access permissions to include the ADMIN role for all event-related operations. This change ensures that ADMIN users have the necessary access to manage events, aligning with updated role-based access control requirements.

--- src/app/modules/event/event.routes.ts
+++ src/app/modules/event/event.routes.ts
@@ -7,9 +7,9 @@ import { USER_ROLES } from '../../../enums/user';
 
 const router = express.Router();
 
-router.post('/create', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.USER), EventController.createEvent);
-router.get('/', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.USER), EventController.getAllEvents);
-router.get('/:id', auth(USER_ROLES.USER, USER_ROLES.SUPER_ADMIN), EventController.getSingleEvent);
-router.patch("/:id", auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.USER), EventController.updateEvent);
-router.delete("/:id", auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.USER), EventController.deleteEvent);
+router.post('/create', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.USER,USER_ROLES.ADMIN), EventController.createEvent);
+router.get('/', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.USER, USER_ROLES.ADMIN), EventController.getAllEvents);
+router.get('/:id', auth(USER_ROLES.USER, USER_ROLES.SUPER_ADMIN,USER_ROLES.ADMIN), EventController.getSingleEvent);
+router.patch("/:id", auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.USER,USER_ROLES.ADMIN), EventController.updateEvent);
+router.delete("/:id", auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.USER,USER_ROLES.ADMIN), EventController.deleteEvent);
 export const EventRoutes = router;
