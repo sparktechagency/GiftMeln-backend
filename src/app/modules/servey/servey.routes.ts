f84d55f774c394a26830c2b38614552f18562772
iamrazzakk <rakibt23p@gmail.com>
2025-05-03 15:48:57 +0600

    feat(servey): allow admin and super admin roles to access survey routes
    Extend access to survey routes for admin and super admin roles to enable broader management capabilities

--- src/app/modules/servey/servey.routes.ts
+++ src/app/modules/servey/servey.routes.ts
@@ -4,13 +4,13 @@ import auth from "../../middlewares/auth";
 import { USER_ROLES } from "../../../enums/user";
 
 const route = Router()
-route.post("/create", auth(USER_ROLES.USER), SurveyController.createSurvey)
+route.post("/create", auth(USER_ROLES.USER, USER_ROLES.ADMIN, USER_ROLES.SUPER_ADMIN), SurveyController.createSurvey)
 
 
-route.patch("/:id", auth(USER_ROLES.USER), SurveyController.updateSurvey)
+route.patch("/:id", auth(USER_ROLES.USER, USER_ROLES.ADMIN, USER_ROLES.SUPER_ADMIN), SurveyController.updateSurvey)
 
 
-route.get("/:id", auth(USER_ROLES.USER), SurveyController.getAllSurveys)
+route.get("/:id", auth(USER_ROLES.USER, USER_ROLES.ADMIN, USER_ROLES.SUPER_ADMIN), SurveyController.getAllSurveys)
 
 // export route
 export const SurveyRoute = route
