d00924392bea459479c87ca13139a627211121c8
iamrazzakk <rakibt23p@gmail.com>
2025-05-19 15:51:57 +0600

    style(payment.model): standardize string quotes and improve formatting
    This commit standardizes the use of single quotes for string literals and improves the readability of the `status` enum by breaking it into multiple lines. These changes enhance code consistency and maintainability.

--- src/app/modules/payment/payment.model.ts
+++ src/app/modules/payment/payment.model.ts
@@ -1,19 +1,21 @@
-import { Schema, model, Types } from "mongoose";
-import { IPayment } from "./payment.interface";
+import { Schema, model, Types } from 'mongoose';
+import { IPayment } from './payment.interface';
 
 const PaymentSchema = new Schema<IPayment>({
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   customerId: { type: String, required: true },
-  package: { type: Schema.Types.ObjectId, ref: "Package", required: true },
+  package: { type: Schema.Types.ObjectId, ref: 'Package', required: true },
   trxId: { type: String },
   subscriptionId: { type: String, required: true },
   currentPeriodStart: { type: Date, required: true },
   currentPeriodEnd: { type: Date, required: true },
   amountPaid: { type: Number, required: true },
-  status: { type: String, enum: ["active", "canceled", "expired"], required: true },
-  paymentType: { type: String, enum: ["subscription"], required: true },
+  status: {
+    type: String,
+    enum: ['active', 'canceled', 'expired'],
+    required: true,
+  },
+  paymentType: { type: String, enum: ['subscription'], required: true },
 });
 
-
-
-export const Subscription = model<IPayment>("Subscription", PaymentSchema);
+export const Subscription = model<IPayment>('Subscription', PaymentSchema);
