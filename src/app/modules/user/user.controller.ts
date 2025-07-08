c732e9e90f63d91810c1fbd274ec891baf459fee
iamrazzakk <rakibt23p@gmail.com>
2025-07-08 09:11:33 +0600

    refactor(user): improve code formatting and consistency in user controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,22 +1,20 @@
-import {  Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import catchAsync from '../../../shared/catchAsync';
 import { getSingleFilePath } from '../../../shared/getFilePath';
 import sendResponse from '../../../shared/sendResponse';
 import { UserService } from './user.service';
 
-const createUser = catchAsync(
-  async (req: Request, res: Response) => {
-    const { ...userData } = req.body;
-    const result = await UserService.createUserToDB(userData);
-    sendResponse(res, {
-      success: true,
-      statusCode: StatusCodes.OK,
-      message: 'User created successfully',
-      data: {},
-    });
-  }
-);
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const { ...userData } = req.body;
+  await UserService.createUserToDB(userData);
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: 'User created successfully',
+    data: {},
+  });
+});
 
 const getUserProfile = catchAsync(async (req: Request, res: Response) => {
   const user = req.user;
@@ -31,25 +29,23 @@ const getUserProfile = catchAsync(async (req: Request, res: Response) => {
 });
 
 //update profile
-const updateProfile = catchAsync(
-  async (req: Request, res: Response) => {
-    const user = req.user;
-    const image = getSingleFilePath(req.files, 'image');
+const updateProfile = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user;
+  const image = getSingleFilePath(req.files, 'image');
 
-    const data = {
-      image,
-      ...req.body,
-    };
-    const result = await UserService.updateProfileToDB(user!, data);
+  const data = {
+    image,
+    ...req.body,
+  };
+  const result = await UserService.updateProfileToDB(user!, data);
 
-    sendResponse(res, {
-      success: true,
-      statusCode: StatusCodes.OK,
-      message: 'Profile updated successfully',
-      data: result,
-    });
-  }
-);
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: 'Profile updated successfully',
+    data: result,
+  });
+});
 
 const getAllAdmins = catchAsync(async (req: Request, res: Response) => {
   const result = await UserService.getAllAdminsFromDB();
@@ -62,4 +58,9 @@ const getAllAdmins = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-export const UserController = { createUser, getUserProfile, updateProfile, getAllAdmins };
+export const UserController = {
+  createUser,
+  getUserProfile,
+  updateProfile,
+  getAllAdmins,
+};
