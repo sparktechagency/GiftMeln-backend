cf94caa6e6d253566aa7b70f6ce9c2abf29d188e
iamrazzakk <rakibt23p@gmail.com>
2025-07-10 10:12:33 +0600

    fix(wishlist): remove undefined types from IwishlistItems interface for user and product

--- src/app/modules/wishlist/wishlist.interface.ts
+++ src/app/modules/wishlist/wishlist.interface.ts
@@ -1,6 +1,6 @@
 import { Types } from 'mongoose';
 
 export type IwishlistItems = {
-  user: Types.ObjectId | undefined;
-  product: Types.ObjectId | undefined;
+  user: Types.ObjectId ;
+  product: Types.ObjectId ;
 };
